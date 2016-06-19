300a16463b352bb7830752f778e08eee66aa1ea7
Miel Donkers <miel.donkers@codecentric.nl>
2016-06-19 21:45:37 +0200

    Add some missing files and further cleanup

---
+++ bin/install.sh
@@ -0,0 +1,582 @@
+#!/bin/bash
+set -e
+
+# install.sh
+#	This script installs my basic setup for a debian laptop
+
+# get the user that is not root
+# TODO: makes a pretty bad assumption that there is only one other user
+USERNAME=$(find /home/* -maxdepth 0 -printf "%f" -type d)
+export DEBIAN_FRONTEND=noninteractive
+
+check_is_sudo() {
+	if [ "$EUID" -ne 0 ]; then
+		echo "Please run as root."
+		exit
+	fi
+}
+
+# sets up apt sources
+# assumes you are going to use debian stretch
+setup_sources() {
+	apt-get update
+	apt-get install -y \
+		apt-transport-https \
+		--no-install-recommends
+
+	cat <<-EOF > /etc/apt/sources.list
+	deb http://httpredir.debian.org/debian stretch main contrib non-free
+	deb-src http://httpredir.debian.org/debian/ stretch main contrib non-free
+
+	deb http://httpredir.debian.org/debian/ stretch-updates main contrib non-free
+	deb-src http://httpredir.debian.org/debian/ stretch-updates main contrib non-free
+
+	deb http://security.debian.org/ stretch/updates main contrib non-free
+	deb-src http://security.debian.org/ stretch/updates main contrib non-free
+
+	#deb http://httpredir.debian.org/debian/ jessie-backports main contrib non-free
+	#deb-src http://httpredir.debian.org/debian/ jessie-backports main contrib non-free
+
+	deb http://httpredir.debian.org/debian experimental main contrib non-free
+	deb-src http://httpredir.debian.org/debian experimental main contrib non-free
+
+	# hack for latest git (don't judge)
+	deb http://ppa.launchpad.net/git-core/ppa/ubuntu wily main
+	deb-src http://ppa.launchpad.net/git-core/ppa/ubuntu wily main
+
+	# neovim
+	deb http://ppa.launchpad.net/neovim-ppa/unstable/ubuntu wily main
+	deb-src http://ppa.launchpad.net/neovim-ppa/unstable/ubuntu wily main
+
+	# tlp: Advanced Linux Power Management
+	# http://linrunner.de/en/tlp/docs/tlp-linux-advanced-power-management.html
+	deb http://repo.linrunner.de/debian sid main
+	EOF
+
+	# add docker apt repo
+	cat <<-EOF > /etc/apt/sources.list.d/docker.list
+	deb https://apt.dockerproject.org/repo debian-stretch main
+	deb https://apt.dockerproject.org/repo debian-stretch testing
+	deb https://apt.dockerproject.org/repo debian-stretch experimental
+	EOF
+
+	# add docker gpg key
+	apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+	# add the git-core ppa gpg key
+	apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys E1DD270288B4E6030699E45FA1715D88E1DF1F24
+
+	# add the neovim ppa gpg key
+	apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 9DBB0BE9366964F134855E2255F96FCF8231B6DD
+
+	# add the tlp apt-repo gpg key
+	apt-key adv --keyserver pool.sks-keyservers.net --recv-keys CD4E8809
+
+	# turn off translations, speed up apt-get update
+	mkdir -p /etc/apt/apt.conf.d
+	echo 'Acquire::Languages "none";' > /etc/apt/apt.conf.d/99translations
+}
+
+# installs base packages
+# the utter bare minimal shit
+base() {
+	apt-get update
+	apt-get -y upgrade
+
+	apt-get install -y \
+		adduser \
+		alsa-utils \
+		apparmor \
+		automake \
+		bash-completion \
+		bc \
+		bridge-utils \
+		bzip2 \
+		ca-certificates \
+		cgroupfs-mount \
+		coreutils \
+		curl \
+		dnsutils \
+		file \
+		findutils \
+		gcc \
+		git \
+		gnupg \
+		grep \
+		gzip \
+		hostname \
+		indent \
+		iptables \
+		jq \
+		less \
+		libapparmor-dev \
+		libc6-dev \
+		libltdl-dev \
+		libseccomp-dev \
+		locales \
+		lsof \
+		make \
+		mount \
+		net-tools \
+		network-manager \
+		openvpn \
+		rxvt-unicode-256color \
+		s3cmd \
+		scdaemon \
+		silversearcher-ag \
+		ssh \
+		strace \
+		sudo \
+		tar \
+		tree \
+		tzdata \
+		unzip \
+		xclip \
+		xcompmgr \
+		xz-utils \
+		zip \
+		--no-install-recommends
+
+	# install tlp with recommends
+	apt-get install -y tlp tlp-rdw
+
+	setup_sudo
+
+	apt-get autoremove
+	apt-get autoclean
+	apt-get clean
+
+	install_docker
+	install_scripts
+	install_syncthing
+}
+
+# setup sudo for a user
+# because fuck typing that shit all the time
+# just have a decent password
+# and lock your computer when you aren't using it
+# if they have your password they can sudo anyways
+# so its pointless
+# i know what the fuck im doing ;)
+setup_sudo() {
+	# add user to sudoers
+	adduser "$USERNAME" sudo
+
+	# add user to systemd groups
+	# then you wont need sudo to view logs and shit
+	gpasswd -a "$USERNAME" systemd-journal
+	gpasswd -a "$USERNAME" systemd-network
+
+	# add go path to secure path
+	{ \
+		echo -e 'Defaults	secure_path="/usr/local/go/bin:/home/jessie/.go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"'; \
+		echo -e 'Defaults	env_keep += "ftp_proxy http_proxy https_proxy no_proxy GOPATH EDITOR"'; \
+		echo -e "${USERNAME} ALL=(ALL) NOPASSWD:ALL"; \
+		echo -e "${USERNAME} ALL=NOPASSWD: /sbin/ifconfig, /sbin/ifup, /sbin/ifdown, /sbin/ifquery"; \
+	} >> /etc/sudoers
+
+	# setup downloads folder as tmpfs
+	# that way things are removed on reboot
+	# i like things clean but you may not want this
+	mkdir -p "/home/$USERNAME/Downloads"
+	echo -e "\n# tmpfs for downloads\ntmpfs\t/home/${USERNAME}/Downloads\ttmpfs\tnodev,nosuid,size=2G\t0\t0" >> /etc/fstab
+}
+
+# installs docker master
+# and adds necessary items to boot params
+install_docker() {
+	# create docker group
+	sudo groupadd docker
+	sudo gpasswd -a "$USERNAME" docker
+
+
+	curl -sSL https://get.docker.com/builds/Linux/x86_64/docker-latest.tgz | tar -xvz \
+		-C /usr/local/bin --strip-components 1
+	chmod +x /usr/local/bin/docker*
+
+	curl -sSL https://raw.githubusercontent.com/jfrazelle/dotfiles/master/etc/systemd/system/docker.service > /etc/systemd/system/docker.service
+	curl -sSL https://raw.githubusercontent.com/jfrazelle/dotfiles/master/etc/systemd/system/docker.socket > /etc/systemd/system/docker.socket
+
+	systemctl daemon-reload
+	systemctl enable docker
+
+	# update grub with docker configs and power-saving items
+	sed -i.bak 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1 i915.enable_psr=0 pcie_asm=force i915.i915_enable_fbc=1 i915.i915_enable_rc6=7 i915.lvds_downclock=1 apparmor=1 security=apparmor"/g' /etc/default/grub
+	echo "Docker has been installed. If you want memory management & swap"
+	echo "run update-grub & reboot"
+}
+
+# install/update golang from source
+install_golang() {
+	export GO_VERSION=1.6.2
+	export GO_SRC=/usr/local/go
+
+	# if we are passing the version
+	if [[ ! -z "$1" ]]; then
+		export GO_VERSION=$1
+	fi
+
+	# purge old src
+	if [[ -d "$GO_SRC" ]]; then
+		sudo rm -rf "$GO_SRC"
+		sudo rm -rf "$GOPATH"
+	fi
+
+	# subshell because we `cd`
+	(
+	curl -sSL "https://storage.googleapis.com/golang/go${GO_VERSION}.linux-amd64.tar.gz" | sudo tar -v -C /usr/local -xz
+	)
+
+	# get commandline tools
+	(
+	set -x
+	set +e
+	go get github.com/golang/lint/golint
+	go get golang.org/x/tools/cmd/cover
+	go get golang.org/x/review/git-codereview
+	go get golang.org/x/tools/cmd/goimports
+	go get golang.org/x/tools/cmd/gorename
+	go get golang.org/x/tools/cmd/guru
+
+	go get github.com/jfrazelle/apk-file
+	go get github.com/jfrazelle/bane
+	go get github.com/jfrazelle/battery
+	go get github.com/jfrazelle/cliaoke
+	go get github.com/jfrazelle/magneto
+	go get github.com/jfrazelle/netns
+	go get github.com/jfrazelle/netscan
+	go get github.com/jfrazelle/onion
+	go get github.com/jfrazelle/pastebinit
+	go get github.com/jfrazelle/pony
+	go get github.com/jfrazelle/riddler
+	go get github.com/jfrazelle/udict
+	go get github.com/jfrazelle/weather
+
+	go get github.com/axw/gocov/gocov
+	go get github.com/brianredbeard/gpget
+	go get github.com/cloudflare/cfssl/cmd/cfssl
+	go get github.com/cloudflare/cfssl/cmd/cfssljson
+	go get github.com/crosbymichael/gistit
+	go get github.com/crosbymichael/ip-addr
+	go get github.com/cbednarski/hostess/cmd/hostess
+	go get github.com/FiloSottile/gvt
+	go get github.com/FiloSottile/vendorcheck
+	go get github.com/nsf/gocode
+	go get github.com/rogpeppe/godef
+	go get github.com/shurcooL/git-branches
+	go get github.com/shurcooL/gostatus
+	go get github.com/shurcooL/markdownfmt
+	go get github.com/Soulou/curl-unix-socket
+
+	aliases=( cloudflare/cfssl docker/docker kubernetes/kubernetes letsencrypt/boulder opencontainers/runc jfrazelle/binctr jfrazelle/contained.af )
+	for project in "${aliases[@]}"; do
+		owner=$(dirname "$project")
+		repo=$(basename "$project")
+		if [[ -d "${HOME}/${repo}" ]]; then
+			rm -rf "${HOME}/${repo}"
+		fi
+
+		mkdir -p "${GOPATH}/src/github.com/${owner}"
+
+		if [[ ! -d "${GOPATH}/src/github.com/${project}" ]]; then
+			(
+			# clone the repo
+			cd "${GOPATH}/src/github.com/${owner}"
+			git clone "https://github.com/${project}.git"
+			# fix the remote path, since our gitconfig will make it git@
+			cd "${GOPATH}/src/github.com/${project}"
+			git remote set-url origin "https://github.com/${project}.git"
+			)
+		else
+			echo "found ${project} already in gopath"
+		fi
+
+		# make sure we create the right git remotes
+		if [[ "$owner" != "jfrazelle" ]]; then
+			(
+			cd "${GOPATH}/src/github.com/${project}"
+			git remote set-url --push origin no_push
+			git remote add jfrazelle "https://github.com/jfrazelle/${repo}.git"
+			)
+		fi
+
+		# create the alias
+		ln -snvf "${GOPATH}/src/github.com/${project}" "${HOME}/${repo}"
+	done
+
+	# create symlinks from personal projects to
+	# the ${HOME} directory
+	projectsdir=$GOPATH/src/github.com/jfrazelle
+	base=$(basename "$projectsdir")
+	find "$projectsdir" -maxdepth 1 -not -name "$base" -type d -print0 | while read -d '' -r dir; do
+	base=$(basename "$dir")
+	ln -snvf "$dir" "${HOME}/${base}"
+done
+)
+}
+
+# install graphics drivers
+install_graphics() {
+	local system=$1
+
+	if [[ -z "$system" ]]; then
+		echo "You need to specify whether it's dell, mac or lenovo"
+		exit 1
+	fi
+
+	local pkgs="nvidia-kernel-dkms bumblebee-nvidia primus"
+
+	if [[ $system == "mac" ]] || [[ $system == "dell" ]]; then
+		local pkgs="xorg xserver-xorg xserver-xorg-video-intel"
+	fi
+
+	apt-get install -y $pkgs --no-install-recommends
+}
+
+# install custom scripts/binaries
+install_scripts() {
+	# install acsciinema
+	curl -sSL https://asciinema.org/install | sh
+
+	# install speedtest
+	curl -sSL https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest_cli.py > /usr/local/bin/speedtest
+	chmod +x /usr/local/bin/speedtest
+
+	# install icdiff
+	curl -sSL https://raw.githubusercontent.com/jeffkaufman/icdiff/master/icdiff > /usr/local/bin/icdiff
+	curl -sSL https://raw.githubusercontent.com/jeffkaufman/icdiff/master/git-icdiff > /usr/local/bin/git-icdiff
+	chmod +x /usr/local/bin/icdiff
+	chmod +x /usr/local/bin/git-icdiff
+
+	# install lolcat
+	curl -sSL https://raw.githubusercontent.com/tehmaze/lolcat/master/lolcat > /usr/local/bin/lolcat
+	chmod +x /usr/local/bin/lolcat
+
+	# download syncthing binary
+	if [[ ! -f /usr/local/bin/syncthing ]]; then
+		curl -sSL https://jesss.s3.amazonaws.com/binaries/syncthing > /usr/local/bin/syncthing
+		chmod +x /usr/local/bin/syncthing
+	fi
+
+	syncthing -upgrade
+
+	local scripts=( go-md2man have light )
+
+	for script in "${scripts[@]}"; do
+		curl -sSL "http://jesss.s3.amazonaws.com/binaries/$script" > /usr/local/bin/$script
+		chmod +x /usr/local/bin/$script
+	done
+}
+
+# install syncthing
+install_syncthing() {
+	curl -sSL https://raw.githubusercontent.com/jfrazelle/dotfiles/master/etc/systemd/system/syncthing@.service > /etc/systemd/system/syncthing@.service
+
+	systemctl daemon-reload
+	systemctl enable "syncthing@${USERNAME}"
+}
+
+# install wifi drivers
+install_wifi() {
+	local system=$1
+
+	if [[ -z "$system" ]]; then
+		echo "You need to specify whether it's broadcom or intel"
+		exit 1
+	fi
+
+	if [[ $system == "broadcom" ]]; then
+		local pkg="broadcom-sta-dkms"
+
+		apt-get install -y "$pkg" --no-install-recommends
+	else
+		update-iwlwifi
+	fi
+}
+
+# install stuff for i3 window manager
+install_wmapps() {
+	local pkgs="feh i3 i3lock i3status scrot slim neovim"
+
+	apt-get install -y $pkgs --no-install-recommends
+
+	# update clickpad settings
+	mkdir -p /etc/X11/xorg.conf.d/
+	curl -sSL https://raw.githubusercontent.com/jfrazelle/dotfiles/master/etc/X11/xorg.conf.d/50-synaptics-clickpad.conf > /etc/X11/xorg.conf.d/50-synaptics-clickpad.conf
+
+	# add xorg conf
+	curl -sSL https://raw.githubusercontent.com/jfrazelle/dotfiles/master/etc/X11/xorg.conf > /etc/X11/xorg.conf
+
+	# get correct sound cards on boot
+	curl -sSL https://raw.githubusercontent.com/jfrazelle/dotfiles/master/etc/modprobe.d/intel.conf > /etc/modprobe.d/intel.conf
+
+	# pretty fonts
+	curl -sSL https://raw.githubusercontent.com/jfrazelle/dotfiles/master/etc/fonts/local.conf > /etc/fonts/local.conf
+
+	echo "Fonts file setup successfully now run:"
+	echo "	dpkg-reconfigure fontconfig-config"
+	echo "with settings: "
+	echo "	Autohinter, Automatic, No."
+	echo "Run: "
+	echo "	dpkg-reconfigure fontconfig"
+}
+
+get_dotfiles() {
+	# create subshell
+	(
+	cd "/home/$USERNAME"
+
+	# install dotfiles from repo
+	git clone git@github.com:jfrazelle/dotfiles.git "/home/$USERNAME/dotfiles"
+	cd "/home/$USERNAME/dotfiles"
+
+	# installs all the things
+	make
+
+	# enable dbus for the user session
+	# systemctl --user enable dbus.socket
+
+	sudo systemctl enable i3lock
+	sudo systemctl enable suspend-sedation.service
+
+	cd "/home/$USERNAME"
+
+	# install .vim files
+	git clone --recursive git@github.com:jfrazelle/.vim.git "/home/$USERNAME/.vim"
+	ln -snf "/home/$USERNAME/.vim/vimrc" "/home/$USERNAME/.vimrc"
+	sudo ln -snf "/home/$USERNAME/.vim" /root/.vim
+	sudo ln -snf "/home/$USERNAME/.vimrc" /root/.vimrc
+
+	# alias vim dotfiles to neovim
+	mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
+	ln -snf "/home/$USERNAME/.vim" $XDG_CONFIG_HOME/nvim
+	ln -snf "/home/$USERNAME/.vimrc" $XDG_CONFIG_HOME/nvim/init.vim
+	# do the same for root
+	sudo mkdir -p /root/.config
+	sudo ln -snf "/home/$USERNAME/.vim" /root/.config/nvim
+	sudo ln -snf "/home/$USERNAME/.vimrc" /root/.config/nvim/init.vim
+
+	# update alternatives to neovim
+	sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+	sudo update-alternatives --config vi
+	sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+	sudo update-alternatives --config vim
+	sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+	sudo update-alternatives --config editor
+
+	mkdir -p ~/Pictures
+	mkdir -p ~/Torrents
+	)
+}
+
+install_virtualbox() {
+	# check if we need to install libvpx1
+	PKG_OK=$(dpkg-query -W --showformat='${Status}\n' libvpx1 | grep "install ok installed")
+	echo Checking for libvpx1: $PKG_OK
+	if [ "" == "$PKG_OK" ]; then
+		echo "No libvpx1. Installing libvpx1."
+		jessie_sources=/etc/apt/sources.list.d/jessie.list
+		echo "deb http://httpredir.debian.org/debian jessie main contrib non-free" > $jessie_sources
+
+		apt-get update
+		apt-get install -y -t jessie libvpx1 \
+			--no-install-recommends
+
+		# cleanup the file that we used to install things from jessie
+		rm $jessie_sources
+	fi
+
+	echo "deb http://download.virtualbox.org/virtualbox/debian vivid contrib" >> /etc/apt/sources.list.d/virtualbox.list
+	curl -sSL https://www.virtualbox.org/download/oracle_vbox.asc | apt-key add -
+
+	apt-get update
+	apt-get install -y \
+		virtualbox-5.0
+	--no-install-recommends
+}
+
+install_vagrant() {
+	VAGRANT_VERSION=1.8.1
+
+	# if we are passing the version
+	if [[ ! -z "$1" ]]; then
+		export VAGRANT_VERSION=$1
+	fi
+
+	# check if we need to install virtualbox
+	PKG_OK=$(dpkg-query -W --showformat='${Status}\n' virtualbox | grep "install ok installed")
+	echo Checking for virtualbox: $PKG_OK
+	if [ "" == "$PKG_OK" ]; then
+		echo "No virtualbox. Installing virtualbox."
+		install_virtualbox
+	fi
+
+	tmpdir=`mktemp -d`
+	(
+	cd $tmpdir
+	curl -sSL -o vagrant.deb https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/vagrant_${VAGRANT_VERSION}_x86_64.deb
+	dpkg -i vagrant.deb
+	)
+
+	rm -rf $tmpdir
+
+	# install plugins
+	vagrant plugin install vagrant-vbguest
+}
+
+
+usage() {
+	echo -e "install.sh\n\tThis script installs my basic setup for a debian laptop\n"
+	echo "Usage:"
+	echo "  sources                     - setup sources & install base pkgs"
+	echo "  wifi {broadcom,intel}       - install wifi drivers"
+	echo "  graphics {dell,mac,lenovo}  - install graphics drivers"
+	echo "  wm                          - install window manager/desktop pkgs"
+	echo "  dotfiles                    - get dotfiles"
+	echo "  golang                      - install golang and packages"
+	echo "  scripts                     - install scripts"
+	echo "  syncthing                   - install syncthing"
+	echo "  vagrant                     - install vagrant and virtualbox"
+}
+
+main() {
+	local cmd=$1
+
+	if [[ -z "$cmd" ]]; then
+		usage
+		exit 1
+	fi
+
+	if [[ $cmd == "sources" ]]; then
+		check_is_sudo
+
+		# setup /etc/apt/sources.list
+		setup_sources
+
+		base
+	elif [[ $cmd == "wifi" ]]; then
+		install_wifi "$2"
+	elif [[ $cmd == "graphics" ]]; then
+		check_is_sudo
+
+		install_graphics "$2"
+	elif [[ $cmd == "wm" ]]; then
+		check_is_sudo
+
+		install_wmapps
+	elif [[ $cmd == "dotfiles" ]]; then
+		get_dotfiles
+	elif [[ $cmd == "golang" ]]; then
+		install_golang "$2"
+	elif [[ $cmd == "scripts" ]]; then
+		install_scripts
+	elif [[ $cmd == "syncthing" ]]; then
+		install_syncthing
+	elif [[ $cmd == "vagrant" ]]; then
+		install_vagrant "$2"
+	else
+		usage
+	fi
+}
+
+main "$@"
