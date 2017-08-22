0071e8814d4c9b016ed5e2675d3f36425b759b31
Miel Donkers <miel.donkers@codecentric.nl>
2017-08-22 22:42:51 +0200

    Add Keybase sources earlier as root

--- bin/install.sh
+++ bin/install.sh
@@ -63,6 +63,11 @@ setup_sources() {
 	deb https://apt.dockerproject.org/repo debian-stretch experimental
 	EOF
 
+	# add keybase apt repo
+	cat <<-EOF > /etc/apt/sources.list.d/keybase.list
+        deb http://dist.keybase.io/linux/deb/repo/ stable main
+	EOF
+
 	# add docker gpg key
 	apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
@@ -74,6 +79,10 @@ setup_sources() {
 
 	# add the tlp apt-repo gpg key
         apt-key adv --keyserver pool.sks-keyservers.net --recv-keys 02D65EFF
+
+        # add the Keybase gpg key
+        curl https://keybase.io/docs/server_security/code_signing_key.asc | gpg --import
+        gpg --export 222B85B0F90BE2D24CFEB93F47484E50656D16C7 | sudo apt-key add -
 
 	# turn off translations, speed up apt-get update
 	mkdir -p /etc/apt/apt.conf.d
@@ -409,14 +418,6 @@ get_dotfiles() {
 }
 
 install_keybase() {
-	# add keybase apt repo
-	cat <<-EOF > /etc/apt/sources.list.d/keybase.list
-        deb http://dist.keybase.io/linux/deb/repo/ stable main
-	EOF
-
-        curl https://keybase.io/docs/server_security/code_signing_key.asc | gpg --import
-        gpg --export 222B85B0F90BE2D24CFEB93F47484E50656D16C7 | sudo apt-key add -
-
         sudo apt-get update
         sudo apt-get install keybase
         
