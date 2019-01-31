bd5d58f4c737877cde5a2d4c6bedcda81453fd08
Miel Donkers <miel.donkers@instana.com>
2019-01-31 11:23:59 +0100

    Fix AG searching and some cleanup

--- bin/install.sh
+++ bin/install.sh
@@ -453,11 +453,11 @@ get_dotfiles() {
   sudo ln -snf "/home/$USERNAME/.vimrc" /root/.config/nvim/init.vim
 
   # update alternatives to neovim
-  sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+  sudo update-alternatives --install /usr/bin/vi vi "$(command -v nvim)" 60
   sudo update-alternatives --config vi
-  sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+  sudo update-alternatives --install /usr/bin/vim vim "$(command -v nvim)" 60
   sudo update-alternatives --config vim
-  sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+  sudo update-alternatives --install /usr/bin/editor editor "$(command -v nvim)" 60
   sudo update-alternatives --config editor
   )
 }
